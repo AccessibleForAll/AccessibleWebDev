3b7f9493614e338eff7257beb1e29d570b70008d
Emma <emma.dawson@futurice.com>
2022-09-10 20:52:05 +0200

    Clean up comments and console logs

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,39 +5,24 @@ import { BsCircleHalf, BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 import styles from "./Header.module.css"
 import { useState, useRef, useEffect } from "react";
 
-interface ThemeSwitcherProps {
-	setShowThemeSwitcher: (value: boolean) => void
-}
 
 const ThemeSwitcher = () => {
-	// const { setShowThemeSwitcher } = props;
-
 	const [showThemeSwitcher, setShowThemeSwitcher] = useState<boolean>(false)
 	const [currentFocusedEl, setCurrentFocusedEl] = useState<string>("")
-
 
 	const buttonRef = useRef<HTMLButtonElement>(null)
 	const systemRef = useRef<HTMLLIElement>(null)
 	const lightRef = useRef<HTMLLIElement>(null)
 	const darkRef = useRef<HTMLLIElement>(null)
 
-
-
 	const handleThemeSwitcher = () => {
-		console.log("click");
-
 		setShowThemeSwitcher(prevState => !prevState)
-		if (!currentFocusedEl) {
+		if (!currentFocusedEl || currentFocusedEl === "buttonRef") {
 			setCurrentFocusedEl("systemRef")
 		}
-		// if (showThemeSwitcher) {
-		// 	setCurrentFocusedEl("systemRef")
-		// }
 	}
 
 	const handleThemeSwitcherKB = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-		console.log("btn kb");
-
 		if (!showThemeSwitcher && (e.key === "ArrowDown" || e.key === "Down" || e.key === "Enter" || e.key === " ")) {
 			setShowThemeSwitcher(true)
 			setCurrentFocusedEl("systemRef")
@@ -53,44 +38,31 @@ const ThemeSwitcher = () => {
 		}
 	}
 
-	console.log(currentFocusedEl, buttonRef)
 	const handleMenuKBFocus = (e: React.KeyboardEvent<HTMLUListElement>) => {
-		console.log("ul kb");
-
 		if (currentFocusedEl == "systemRef" && (e.key === "ArrowDown" || e.key === "Down")) {
 			setCurrentFocusedEl("lightRef")
-
 		}
 		if (currentFocusedEl == "lightRef" && (e.key === "ArrowDown" || e.key === "Down")) {
 			setCurrentFocusedEl("darkRef")
-
 		}
 		if (currentFocusedEl == "darkRef" && (e.key === "ArrowDown" || e.key === "Down")) {
 			setCurrentFocusedEl("systemRef")
-
 		}
 		if (currentFocusedEl == "systemRef" && (e.key === "ArrowUp" || e.key === "Up")) {
 			setCurrentFocusedEl("darkRef")
-
 		}
 		if (currentFocusedEl == "lightRef" && (e.key === "ArrowUp" || e.key === "Up")) {
 			setCurrentFocusedEl("systemRef")
-
 		}
 		if (currentFocusedEl == "darkRef" && (e.key === "ArrowUp" || e.key === "Up")) {
 			setCurrentFocusedEl("lightRef")
-
 		}
 		if (e.key === "Esc" || e.key === "Escape") {
 			setShowThemeSwitcher(false)
 			buttonRef.current?.focus()
 			setCurrentFocusedEl("buttonRef")
-
 		}
 	}
-
-
-
 
 	const setFocusedElement = (value: string) => {
 		switch (value) {
@@ -108,34 +80,23 @@ const ThemeSwitcher = () => {
 
 	useEffect(() => {
 		if (showThemeSwitcher && currentFocusedEl) {
-			console.log("useEffect");
-
 			setFocusedElement(currentFocusedEl)
 		}
 	}, [showThemeSwitcher, currentFocusedEl])
 
 	const setTheme = (theme: string) => {
-		console.log("setTheme");
-
 		document.documentElement.className = theme
 		localStorage.setItem("theme", theme)
 		buttonRef.current?.focus()
 		setCurrentFocusedEl("buttonRef")
-		//setShowThemeSwitcher(false)
 	};
 
 	const setThemeFromOS = () => {
-		console.log("setDefault");
-
 		window.matchMedia('(prefers-color-scheme: dark)').matches ? document.documentElement.className = "dark" : document.documentElement.className = "light"
 		localStorage.removeItem("theme")
 		buttonRef.current?.focus()
 		setCurrentFocusedEl("buttonRef")
-		//setShowThemeSwitcher(false)
 	};
-
-	console.log(showThemeSwitcher);
-
 
 	let theme;
 	if (typeof window !== "undefined") {
@@ -169,22 +130,6 @@ interface HeaderProps {
 
 const Header = (props: HeaderProps) => {
 	const { pageTitle, headerTitle, handleNavClick, showNavMobile } = props
-	// const [showThemeSwitcher, setShowThemeSwitcher] = useState<boolean>(false)
-
-	// const handleThemeSwitcher = () => {
-	// 	setShowThemeSwitcher(prevState => !prevState)
-	// }
-
-	// const handleThemeSwitcherKB = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-	// 	if (e.key === "ArrowDown" || e.key === "Down") {
-	// 		setShowThemeSwitcher(true)
-	// 	}
-	// 	if (e.key === "Esc" || e.key === "Escape") {
-	// 		setShowThemeSwitcher(false)
-	// 	}
-	// }
-
-	// const buttonRef = useRef<HTMLButtonElement>(null)
 
 	return (
 		<>
@@ -197,18 +142,12 @@ const Header = (props: HeaderProps) => {
 				<div className={styles.topBanner}>
 					<Link href="/">Accessible Web Dev</Link>
 					<div className={styles.buttonsContainer}>
-						{/* <button className={styles.themeBtn} ref={buttonRef} onClick={handleThemeSwitcher} onKeyDown={handleThemeSwitcherKB} type="button" id="themeSwitcherBtn" aria-haspopup="true" aria-controls="themeSwitcherMenu" aria-expanded={showThemeSwitcher}>
-							<BsCircleHalf color="white" size="1rem" aria-hidden="true" />
-							Theme
-						</button>
-						{showThemeSwitcher && <ThemeSwitcher setShowThemeSwitcher={setShowThemeSwitcher} />} */}
 						<ThemeSwitcher />
 						<button className={styles.hamburgerBtn} onClick={handleNavClick} aria-label="Navigation menu" aria-expanded={showNavMobile} >
 							{!showNavMobile && <IoMenuSharp color="white" size="2.5rem" aria-hidden="true" />}
 							{showNavMobile && <IoCloseSharp color="white" size="2.5rem" aria-hidden="true" />}
 						</button>
 					</div>
-
 				</div>
 				<h1 className={styles.headerTitle}>Accessible {headerTitle}</h1>
 			</header>
