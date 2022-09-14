43700e557a42d153e3399d4ed632242429282a4e
Emma <emma.dawson@futurice.com>
2022-09-14 09:25:02 +0200

    Move head tags to each page. Update h1 font size

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -5,29 +5,35 @@ import NavPrimaryMobile from "../Nav/NavPrimaryMobile"
 import styles from "./Layout.module.css"
 
 interface LayoutProps {
-	pageTitle: string
 	headerTitle: string
 	activeNavLink: string
 	children: JSX.Element
 }
 
 const Layout = (props: LayoutProps) => {
-	const { pageTitle, headerTitle, activeNavLink, children } = props
+	const { headerTitle, activeNavLink, children } = props
 
 	const [showNavMobile, setShowNavMobile] = useState<boolean>(false)
 
 	const handleNavClick = () => {
-		setShowNavMobile(prevState => !prevState)
+		setShowNavMobile((prevState) => !prevState)
 	}
-
-
 
 	return (
 		<>
-			<Header pageTitle={pageTitle} headerTitle={headerTitle} handleNavClick={handleNavClick} showNavMobile={showNavMobile} />
+			<Header
+				headerTitle={headerTitle}
+				handleNavClick={handleNavClick}
+				showNavMobile={showNavMobile}
+			/>
 			<div className={styles.layoutContainer}>
 				<NavPrimary activeNavLink={activeNavLink} />
-				{showNavMobile && <NavPrimaryMobile activeNavLink={activeNavLink} handleNavClick={handleNavClick} />}
+				{showNavMobile && (
+					<NavPrimaryMobile
+						activeNavLink={activeNavLink}
+						handleNavClick={handleNavClick}
+					/>
+				)}
 				<main>{children}</main>
 			</div>
 		</>
