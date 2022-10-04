9fb16d66b9138574d6ccf2ca061f4424a793a0f2
Giriraj-Roy <girirajroy2001@gmail.com>
2022-10-04 23:56:54 +0530

    changes made in workInProgress

---
+++ components/WorkInProgress/WorkInProgress.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const WorkInProgress = () => {
+	return (
+		<>
+			<h2>This section is a work in progress</h2>
+			<p>Please come back at a later date</p>
+			<a href="https://github.com/AccessibleForAll/AccessibleWebDev">
+				Let us know what information you're looking for.
+			</a>
+		</>
+	)
+}
+
+export default WorkInProgress
