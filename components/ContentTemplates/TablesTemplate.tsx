d3790aed5ab9610afcfa46b9e6b650ed7a1b2726
huluvu424242 <huluvu424242@gmail.com>
2022-10-06 23:19:46 +0200

    [issue109] placeholder replaced by WIP tag
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- components/ContentTemplates/TablesTemplate.tsx
+++ components/ContentTemplates/TablesTemplate.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import WorkInProgress from "../WorkInProgress/WorkInProgress";
 
 const TablesTemplate = () => {
-	return <div>TablesTemplate</div>
+	return <WorkInProgress />
 }
 
 export default TablesTemplate
