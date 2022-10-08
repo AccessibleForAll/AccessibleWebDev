bc33f3fd26d19fabea9fea556ff17d8c4d54b675
Emma Dawson <emma.l.dawson@gmail.com>
2022-10-08 15:28:24 +0200

    Place image in center

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react"
-import styles from "../styles/pageNotFound.module.css"
 import notFoundImage from "../public/404.png"
 import Image from "next/image"
 import Head from "next/head"
 import Layout from "../components/Layout/Layout"
-import { useRouter } from "next/router"
 
 const PageNotFound = () => {
-	const router = useRouter()
 	return (
 		<>
 			<Head>
@@ -16,16 +13,14 @@ const PageNotFound = () => {
 				<meta name="description" content="404 - Page Not Found" />
 			</Head>
 			<Layout headerTitle="404 - Not Found" activeNavLink={""}>
-				<>
-					<h2>Oops, we could not find this page.</h2>
-
+				<div className="center">
 					<Image
 						src={notFoundImage.src}
 						alt="Page not found"
-						width={400}
-						height={300}
+						width={800}
+						height={600}
 					/>
-				</>
+				</div>
 			</Layout>
 		</>
 	)
