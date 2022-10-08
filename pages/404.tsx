cd565e89025523a8847ecf39fe5067f0ba2303a8
Emma Dawson <emma.l.dawson@gmail.com>
2022-10-08 14:27:38 +0200

    Update 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,64 +2,32 @@
 import React from "react"
 import styles from "../styles/pageNotFound.module.css"
 import notFoundImage from "../public/404.png"
-import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head"
+import Layout from "../components/Layout/Layout"
+import { useRouter } from "next/router"
 
 const PageNotFound = () => {
+	const router = useRouter()
 	return (
-		<main className={styles.notFound}>
-			<div className={styles.left}>
-				<h1 className={styles.headingPrimary}>
-					Oops, we couldn't find this page.
-				</h1>
-				<p className={styles.description}>
-					Here are some helpful links instead:
-				</p>
-				<ul className={styles.linkList}>
-					<li className={styles.linkItems}>
-						<Link href="/">
-							<a className={styles.link}> Home </a>
-						</Link>
-					</li>
-					<li className={styles.linkItems}>
-						<Link href="/animations">
-							<a className={styles.link}>Animations</a>
-						</Link>
-					</li>
-					<li className={styles.linkItems}>
-						<Link href="/audio">
-							<a className={styles.link}>Audio</a>
-						</Link>
-					</li>
-					<li className={styles.linkItems}>
-						<Link href="/captchas">
-							<a className={styles.link}>Captchas</a>
-						</Link>
-					</li>
-					<li className={styles.linkItems}>
-						<Link href="/charts">
-							<a className={styles.link}>Charts</a>
-						</Link>
-					</li>
-					<li className={styles.linkItems}>
-						<Link href="/icons">
-							<a className={styles.link}>Icons</a>
-						</Link>
-					</li>
-					<li className={styles.linkItems}>
-						<Link href="/images">
-							<a className={styles.link}>Images</a>
-						</Link>
-					</li>
-				</ul>
-			</div>
-			<div className={styles.right}>
-				<img
-					src={notFoundImage.src}
-					className={styles.notFoundImage}
-					alt="not found"
-				/>
-			</div>
-		</main>
+		<>
+			<Head>
+				<title>{`Page Not Found - Accessible Web Dev`}</title>
+				<meta name="description" content="404 - Page Not Found" />
+			</Head>
+			<Layout headerTitle="404 - Not Found" activeNavLink={""}>
+				<>
+					<h2>Oops, we could not find this page.</h2>
+
+					<Image
+						src={notFoundImage.src}
+						alt="Page not found"
+						width={400}
+						height={300}
+					/>
+				</>
+			</Layout>
+		</>
 	)
 }
 
