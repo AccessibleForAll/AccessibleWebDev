8088f09486db716eb47dbdfafad0a6d13b6c2a1b
Peter <petecruckshank@gmail.com>
2022-10-08 20:49:21 -0400

    Update Next config file to load i18n settings

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const { i18n } = require("./next-i18next.config")
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	i18n: {
-		locales: ["en", "sv"],
-		defaultLocale: "en",
-	},
 }
 
 module.exports = nextConfig
