3c33cf760a92abfab66abf3fc3781d22c2d568fc
PeroHub <peterime600@gmail.com>
2022-10-10 11:33:35 +0100

    importeed work in progress into charts template

--- components/ContentTemplates/ChartsTemplate.tsx
+++ components/ContentTemplates/ChartsTemplate.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import WorkInProgress from "../WorkInProgress/WorkInProgress"
 
 const ChartsTemplate = () => {
-	return <div>ChartsTemplate</div>
+	return <WorkInProgress />
 }
 
 export default ChartsTemplate
