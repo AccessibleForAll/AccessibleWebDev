31cca499102167be715f03c6304288e25c32bee7
Emma Dawson <emma.l.dawson@gmail.com>
2022-10-14 19:25:03 +0200

    Add en and sv translations to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,28 +15,14 @@ const Home: NextPage = () => {
 			</Head>
 			<Layout headerTitle="Accessible Web Dev" activeNavLink="/">
 				<section>
-					<h2>{t("welcome")}</h2>
+					<h2>{t("heading.welcome")}</h2>
+					<p>{t("para.text1")}</p>
+					<p>{t("para.text2")}</p>
+					<p>{t("para.text3")}</p>
 					<p>
-						Accessible web dev is here to make accessibility more understandable
-						for developers and designers as current standards can be hard to
-						understand.
-					</p>
-					<p>
-						This is not a replacement for current standards but hopefully a
-						complement to them and help on your journey to creating more
-						accessible web experiences for everyone.
-					</p>
-					<p>
-						Each page will contain a variety of examples of how to make specific
-						elements or widgets accessible. It will also provide links to
-						resources and further reading.
-					</p>
-					<p>
-						It is currently a work in progress so not all pages are finished (or
-						even started). If you don't find what you are looking for please
-						feel free to{" "}
+						{t("para.text4")}{" "}
 						<a href="https://github.com/AccessibleForAll/AccessibleWebDev/issues/new/choose">
-							raise an issue on GitHub.
+							{t("link.githubIssue")}
 						</a>
 					</p>
 				</section>
