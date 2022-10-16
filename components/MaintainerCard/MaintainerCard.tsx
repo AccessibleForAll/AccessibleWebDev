0e92a2d7d6ea8d1d8ca8166a41926125a0ef3376
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-10-16 16:04:05 -0300

    fix: adding correct file this time

--- components/MaintainerCard/MaintainerCard.tsx
+++ components/MaintainerCard/MaintainerCard.tsx
@@ -1,1 +1,48 @@
-export {}
+import Image from "next/image"
+import styles from "./MaintainerCard.module.css"
+
+const isLinkToImage = (link = "default") =>
+	/\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(link)
+
+export interface MaintainerCardProps {
+	maintainerImage: string
+	maintainerName: string
+	maintainerSurname: string
+	maintainerDescription: string
+	maintainerGithubName: string
+	maintainerGithubLink: string
+}
+
+export const MaintainerCard = (props: MaintainerCardProps) => {
+	const {
+		maintainerImage,
+		maintainerName,
+		maintainerSurname,
+		maintainerDescription,
+		maintainerGithubName,
+		maintainerGithubLink,
+	} = props
+
+	return (
+		<section>
+			{isLinkToImage(maintainerImage) ? (
+				<Image
+					src={maintainerImage}
+					alt={`${maintainerName} ${maintainerSurname}`}
+					layout="fill"
+				/>
+			) : (
+				<p>{maintainerImage || "image"}</p>
+			)}
+
+			<h1>{`${maintainerName} ${maintainerSurname}`}</h1>
+			<p>{maintainerDescription}</p>
+
+			<a href={maintainerGithubLink} target="_blank" rel="noreferrer">
+				<button type="button">{maintainerGithubName}</button>
+			</a>
+		</section>
+	)
+}
+
+export default MaintainerCard
