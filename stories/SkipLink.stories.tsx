abc24455a2f839453606e3c32893a28ac9d74fa3
Haris <harisshah385@gmail.com>
2022-10-19 22:16:35 +0500

    Add skiplink story

---
+++ stories/SkipLink.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { SkipLink } from "../components/SkipLink/SkipLink"
+
+export default {
+	title: "Example/Header",
+	component: SkipLink,
+} as ComponentMeta<typeof SkipLink>
+
+const Template: ComponentStory<typeof SkipLink> = () => <SkipLink />
+
+export const HiddenSkipLink = Template.bind({})
