b3724fa946a321fa89c14275afa79ba0c119acf2
Julia Undeutsch <JUndeutsch@gmx.at>
2022-10-19 20:56:06 +0200

    install a11y extension storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
 		"@storybook/addon-interactions",
 		"storybook-addon-next",
 		"storybook-css-modules",
+		"@storybook/addon-a11y",
 	],
 	framework: "@storybook/react",
 	core: {
