95345c60d5164963d97ef427e91ef58551f8f469
Emma Dawson <emma.l.dawson@gmail.com>
2022-11-20 10:22:02 +0100

    Add in-page navigation component

---
+++ components/NavPage/NavPage.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+import styles from "./NavPage.module.css"
+import { IPageNavigationItem } from "../../data/pageNavigationLists"
+
+interface INavPageProps {
+	pageNavigation: IPageNavigationItem[]
+}
+
+export const NavPage = ({ pageNavigation }: INavPageProps) => {
+	return (
+		<details className={styles.pageNav}>
+			<summary className={styles.pageNavTitle}>On This Page</summary>
+
+			<nav aria-label="On This Page">
+				<ul className={styles.pageNavList}>
+					{pageNavigation.map((item, index) => (
+						<li key={index}>
+							<Link href={item.href}>{item.linkName}</Link>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</details>
+	)
+}
+
+export default NavPage
