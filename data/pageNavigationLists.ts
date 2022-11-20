8552d9f65014fcc18ce63ed78de55e22f8f5c1c7
Emma Dawson <emma.l.dawson@gmail.com>
2022-11-20 10:18:03 +0100

    Add pageNavigationLists to data

---
+++ data/pageNavigationLists.ts
@@ -0,0 +1,19 @@
+export interface IPageNavigationItem {
+	linkName: string
+	href: string
+}
+
+export const imagePageNavigation: IPageNavigationItem[] = [
+	{ linkName: "Introduction", href: "#introduction" },
+	{ linkName: "Text Alternatives", href: "#textAlternatives" },
+	{ linkName: "Decorative Images", href: "#decorativeImages" },
+	{ linkName: "Informative Images", href: "#informativeImages" },
+	{ linkName: "Functional Images", href: "#functionalImages" },
+	{ linkName: "How to write good alt text", href: "#goodAltText" },
+	{ linkName: "Images of Text", href: "#imagesOfText" },
+	{ linkName: "Images and Colour", href: "#imagesAndColour" },
+	{ linkName: "Image Maps", href: "#imageMaps" },
+	{ linkName: "Image Checklist", href: "#checklist" },
+	{ linkName: "WCAG Criteria", href: "#WCAGCriteria" },
+	{ linkName: "Other Resources", href: "#otherResources" },
+]
