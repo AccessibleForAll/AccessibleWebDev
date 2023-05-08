82a325a2d932d20e2868f9226478efff0fdd6d56
NikhilGithubOfficial <nkmnikhil0009@gmail.com>
2023-05-08 11:47:49 +0530

    wrapped entire toggle in div

--- customHooks/useOnClickOutside.ts
+++ customHooks/useOnClickOutside.ts
@@ -4,17 +4,15 @@ import { useEventListener } from "usehooks-ts"
 type Handler = (event: MouseEvent) => void
 
 function useOnClickOutside<T extends HTMLElement = HTMLElement>(
-	ulref: RefObject<T>,
-	buttonref: RefObject<HTMLButtonElement>,
+	ref: RefObject<T>,
 	handler: Handler,
 	mouseEvent: "mousedown" | "mouseup" = "mousedown"
 ): void {
 	useEventListener(mouseEvent, (event) => {
+		const el = ref?.current
+
 		// Do nothing if clicking ref's element or descendent elements
-		if (
-			ulref?.current?.contains(event.target as Node) ||
-			buttonref?.current?.contains(event.target as Node)
-		) {
+		if (!el || el.contains(event.target as Node)) {
 			return
 		}
 
