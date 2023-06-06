d83033411a31207c9b270e9d92fde44af33e3d54
Emma Dawson <emma.l.dawson@gmail.com>
2023-06-06 15:58:49 +0200

    Add keyboard accessibility to code block

--- components/CodeBlock/CodeBlock.tsx
+++ components/CodeBlock/CodeBlock.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/prism"
 import CopyCodeBlock from "../CopyCodeBlock/CopyCodeBlock"
@@ -16,13 +17,29 @@ interface ICodeBlockProps {
 }
 
 export const CodeBlock = ({ codeSnippet, languageType }: ICodeBlockProps) => {
+	const [scrollableRegion, setScrollableRegion] = useState(false)
+
+	const ref = useRef<HTMLPreElement>(null)
+	const PreWithRef = (preProps: React.HTMLAttributes<HTMLPreElement>) => (
+		<pre {...preProps} ref={ref} />
+	)
+
+	useEffect(() => {
+		if (ref.current) {
+			const element = ref.current
+			setScrollableRegion(element.clientWidth < element.scrollWidth)
+		}
+	}, [])
+
 	return (
 		<div className={style.CodeBlockContainer}>
 			<CopyCodeBlock code={codeSnippet} />
 			<SyntaxHighlighter
 				language={languageType}
 				style={nightOwl}
-				className={style.CodeBlock}>
+				className={style.CodeBlock}
+				PreTag={PreWithRef}
+				tabIndex={scrollableRegion ? 0 : -1}>
 				{codeSnippet}
 			</SyntaxHighlighter>
 		</div>
