c47fcef2c28fce83773428d9980fc8885b9fcb82
DIWAKAR <didk9675@gmail.com>
2023-06-12 21:20:32 +0530

    navigat

--- components/Nav/NavPrimary.tsx
+++ components/Nav/NavPrimary.tsx
@@ -1,22 +1,38 @@
 import { NavItem } from "./NavItem"
 import { pages } from "../../data/pages"
 import styles from "./NavPrimary.module.css"
+import { useState } from "react"
+
+import { AiOutlineSearch } from "react-icons/ai"
 
 export interface INavProps {
 	activeNavLink: string
 }
 
 export const NavPrimary = ({ activeNavLink }: INavProps) => {
+	const [search, setSearch] = useState<string>("")
+
 	return (
 		<nav aria-label="Primary" className={styles.navPrimary}>
+			<input
+				className={styles.searchBox}
+				placeholder="search"
+				type="text"
+				onChange={(e) => setSearch(e.target.value.toLowerCase())}
+			/>
+
 			<ul className={styles.navList}>
-				{pages.map((page, index) => (
-					<NavItem
-						key={page.name + index}
-						page={page}
-						activeNavLink={activeNavLink}
-					/>
-				))}
+				{pages.map((page, index) =>
+					page.name.toLowerCase().includes(search) ? (
+						<NavItem
+							key={page.name + index}
+							page={page}
+							activeNavLink={activeNavLink}
+						/>
+					) : (
+						""
+					)
+				)}
 			</ul>
 		</nav>
 	)
