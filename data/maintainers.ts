93f0c565c0159d8ce8e8151604cdc221ee835a3d
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-07-17 13:19:52 +0530

    perf: Reduced Maintainer's image load time by about 87 %
    Replaced @EmmaDawsonDev's GitHub profile picture link with **https://avatars.githubusercontent.com/u/57045550?v=4&s=150** and @ctoffanin's GitHub profile picture link with **https://avatars.githubusercontent.com/u/1682188?v=4&s=150**. This github profile picture cdn link will provide a **150 x 150** image which does not make any visual difference for the end user, but, it reduces the network payload by about 87%.

--- data/maintainers.ts
+++ data/maintainers.ts
@@ -7,14 +7,14 @@ export interface IMaintainer {
 
 export const currentMaintainers: IMaintainer[] = [
 	{
-		image: "https://github.com/emmadawsondev.png",
+		image: "https://avatars.githubusercontent.com/u/57045550?v=4&s=150",
 		fullName: "Emma Dawson",
 		description:
 			"Emma is a full stack developer from Stockholm with a passion for accessibility and open source. She wants to make web accessibility easy to learn for everyone.",
 		githubLink: "https://github.com/EmmaDawsonDev",
 	},
 	{
-		image: "https://github.com/ctoffanin.png",
+		image: "https://avatars.githubusercontent.com/u/1682188?v=4&s=150",
 		fullName: "Cristian Toffanin",
 		description:
 			"Cristian is a full stack developer based in the Netherlands. He's always curious and always learning. He's currently learning about accessibility (Emma's mentee).",
