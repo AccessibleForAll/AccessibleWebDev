bff177aba9372a73c7bc4c8e818a328ed04afd6f
Christine Belzie <shecoder30@gmail.com>
2023-08-03 17:16:07 -0400

    start setup for audio section

--- components/ContentTemplates/AudioTemplate.tsx
+++ components/ContentTemplates/AudioTemplate.tsx
@@ -1,5 +1,12 @@
-import { WorkInProgress } from "../WorkInProgress/WorkInProgress"
-
+import { audioPageNavigation } from "../../data/pageNavigationLists"
+import { CodeBlock } from "../CodeBlock/CodeBlock"
+import { NavPage } from "../NavPage/NavPage"
+import { PageUpdated } from "../PageUpdated/PageUpdated"
+import { TemplateSection } from "../TemplateSection/TemplateSection"
 export const AudioTemplate = () => {
-	return <WorkInProgress />
+	return (
+		<> <NavPage pageNavigation={audioPageNavigation}
+		<TemplateSection sectionName="introduction"
+		title="Introduction"></TemplateSection>	/>
+	)
 }
