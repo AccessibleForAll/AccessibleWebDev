179897595a3381bd4e95362d8b217c8d046d2189
Christine Belzie <shecoder30@gmail.com>
2023-08-06 19:38:38 -0400

    feat: Started adding some content about adding closed captions to the audio element

--- components/ContentTemplates/AudioTemplate.tsx
+++ components/ContentTemplates/AudioTemplate.tsx
@@ -7,6 +7,25 @@ export const AudioTemplate = () => {
 	return (
 		<> <NavPage pageNavigation={audioPageNavigation}
 		<TemplateSection sectionName="introduction"
-		title="Introduction"></TemplateSection>	/>
+		title="Introduction">
+			</TemplateSection/>
+		<TemplateSection sectionName="howToAdd"
+		title="How To Add Closed Captions To Your Audio?"
+		<p> Whether you are adding an introductory video about yourself on websites or embedded an audio clip of your favorite song to a game you're building, it's important to add closed captions to them. This will ensure that users who are deaf and or have TBIs (Traumatic Brain Injuries) can have an easier viewing experience.
+		 </p>
+		 <p> Example</p>
+		 <CodeBlock
+		 languageType="html"
+		 codeSnippet= {`<video controls width="200">
+  <source src="https://www.youtube.com/watch?v=0QHaxrUkSE" type="video/mp4" />
+  <track
+    default
+    kind="captions"
+    src="https://youtube.com/captions.vtt"
+    srclang="en"
+  />
+</video>`}
+		 />
+		</TemplateSection>
 	)
 }
