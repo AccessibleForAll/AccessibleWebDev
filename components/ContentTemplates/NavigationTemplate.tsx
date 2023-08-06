bd68fc95581c32bf849da2daf905e79ae0b13e45
Emma Dawson <emma.l.dawson@gmail.com>
2023-08-06 15:47:20 +0200

    Add hamburger menu code example

--- components/ContentTemplates/NavigationTemplate.tsx
+++ components/ContentTemplates/NavigationTemplate.tsx
@@ -66,13 +66,92 @@ export const NavigationTemplate = () => {
 					// TODO: Fix this code block!
 				}
 				<CodeBlock
-					codeSnippet={`<button
-onclick={handleNavClick}
-aria-label="Navigation menu"
-aria-expanded={showNavMobile}>
-				
-				`}
+					codeSnippet={`<button 
+	id="hamburgerMenu" class="navButton" 
+	aria-label="Navigation menu" aria-expanded="false" 
+	aria-controls="primaryNav">
+	Menu
+</button>
+<nav id="primaryNav" class="hidden">
+	<ul class="primaryNavList">
+		<li>
+			<a href="#">Item 1</a>
+		</li>
+		<li>
+			<a href="#">Item 2</a>
+		</li>
+		<li>
+			<a href="#">Item 3</a>
+		</li>
+	</ul>
+</nav>`}
 					languageType={"html"}
+				/>
+				<CodeBlock
+					codeSnippet={`.navButton {
+  position: relative;
+}
+
+.primaryNavList {
+  position: absolute;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  border: 1px solid black;
+  width: 10rem;
+  z-index: 1;
+}
+
+.primaryNavList li {
+  padding: 1rem;
+}
+
+.hidden {
+  display: none;
+}`}
+					languageType={"css"}
+				/>
+				<CodeBlock
+					codeSnippet={`const navButton = document.getElementById("hamburgerMenu");
+const disclosure = document.getElementById("primaryNav");
+const listItems = disclosure.querySelectorAll("li a");
+
+function openNavigation() {
+  navButton.setAttribute("aria-expanded", "true");
+  disclosure.classList.remove("hidden");
+}
+
+function closeNavigation() {
+  navButton.setAttribute("aria-expanded", "false");
+  disclosure.classList.add("hidden");
+}
+
+function toggleNavigation() {
+  const open = navButton.getAttribute("aria-expanded");
+  open === "false" ? openNavigation() : closeNavigation();
+}
+
+// This function closes an open disclosure if a user tabs away from the last anchor element in the list. It is reliant on the ul container having a class to check whether the relatedTarget is within the disclosure. If not, it will close.
+function handleBlur() {
+  const navList = event.currentTarget.closest(".primaryNavList");
+  if (!event.relatedTarget || !navList.contains(event.relatedTarget)) {
+    closeNavigation();
+  }
+}
+
+navButton.addEventListener("click", toggleNavigation);
+
+// add event to the last item in the nav list to trigger the disclosure to close if the user tabs out of the disclosure
+listItems[listItems.length - 1].addEventListener("blur", handleBlur);
+
+// Close the disclosure if a user presses the escape key
+window.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    navButton.focus();
+    closeNavigation();
+  }
+});`}
+					languageType={"javascript"}
 				/>
 			</TemplateSection>
 			<TemplateSection sectionName={"megaMenus"} title={"Mega Menus"}>
