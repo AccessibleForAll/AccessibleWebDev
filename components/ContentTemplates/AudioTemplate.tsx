852b7a30e8d19e90ad1d17786b87b8e3650d7855
Christine Belzie <shecoder30@gmail.com>
2023-08-31 16:04:05 -0400

    feat: finished page on audio
    Co-authored-by: Emma Dawson <emma.l.dawson@gmail.com>

--- components/ContentTemplates/AudioTemplate.tsx
+++ components/ContentTemplates/AudioTemplate.tsx
@@ -1,22 +1,33 @@
-import { audioPageNavigation } from "../../data/pageNavigationLists"
 import { CodeBlock } from "../CodeBlock/CodeBlock"
 import { NavPage } from "../NavPage/NavPage"
 import { PageUpdated } from "../PageUpdated/PageUpdated"
 import { TemplateSection } from "../TemplateSection/TemplateSection"
+import { audioPageNavigation } from "../../data/pageNavigationLists"
+
 export const AudioTemplate = () => {
 	return (
-		<> <NavPage pageNavigation={audioPageNavigation}
-		<TemplateSection sectionName="introduction"
-		title="Introduction">
-			</TemplateSection/>
-		<TemplateSection sectionName="howToAdd"
-		title="How To Add Closed Captions To Your Audio?"
-		<p> Whether you are adding an introductory video about yourself on websites or embedded an audio clip of your favorite song to a game you're building, it's important to add closed captions to them. This will ensure that users who are deaf and or have TBIs (Traumatic Brain Injuries) can have an easier viewing experience.
-		 </p>
-		 <p> Example</p>
-		 <CodeBlock
-		 languageType="html"
-		 codeSnippet= {`<video controls width="200">
+		<div>
+			<NavPage pageNavigation={audioPageNavigation} />
+			<TemplateSection sectionName="introduction" title="Introduction">
+				<p>Introduction</p>
+			</TemplateSection>
+			<TemplateSection
+				sectionName="howToAdd"
+				title="How To Add Closed Captions To Your Audio?">
+				<p>
+					Whether you are adding an introductory video about yourself on
+					websites or embedded an audio clip of your favorite song to a game
+					you're building, it's important to add closed captions to them. This
+					will Whether you are adding an introductory video about yourself on
+					websites or embedded an audio clip of your favorite song to a game
+					you're building, it's important to add closed captions to them. This
+					will ensure that users who are deaf and or have TBIs (Traumatic Brain
+					Injuries) can have an easier viewing experience.
+				</p>
+				<p> Example</p>
+				<CodeBlock
+					languageType="html"
+					codeSnippet={`<video controls width="200">
   <source src="https://www.youtube.com/watch?v=0QHaxrUkSE" type="video/mp4" />
   <track
     default
@@ -24,8 +35,10 @@ export const AudioTemplate = () => {
     src="https://youtube.com/captions.vtt"
     srclang="en"
   />
-</video>`}
-		 />
-		</TemplateSection>
+</video>
+`}
+				/>
+			</TemplateSection>
+		</div>
 	)
 }
