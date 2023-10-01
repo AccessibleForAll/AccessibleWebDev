ab2a7e520a8699cf1d52a7f3224c65ca73b55081
DIWAKAR <didk9675@gmail.com>
2023-10-01 20:20:20 +0530

    add searchbar

--- components/Nav/NavPrimary.tsx
+++ components/Nav/NavPrimary.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { NavItem } from "./NavItem"
 import { pages } from "../../data/pages"
 import styles from "./NavPrimary.module.css"
@@ -8,16 +9,29 @@ export interface INavProps {
 
 export const NavPrimary = ({ activeNavLink }: INavProps) => {
 
+	const [search, setSearch] = useState<string>("")
+
 	return (
 		<nav aria-label="Primary" className={styles.navPrimary}>
+
+			<input
+				className={styles.searchBox}
+				placeholder="search"
+				type="text"
+				onChange={(e) => setSearch(e.target.value.toLowerCase())}
+			/>
 			<ul className={styles.navList}>
-				{pages.map((page, index) =>(
-					<NavItem
-					        key={page.name + index}
-					        page={page}
-					        activeNavLink={activeNavLink}
-					/>
-				))}
+				{pages.map((page, index) =>
+					page.name.toLowerCase().includes(search) ? (
+						<NavItem
+							key={page.name + index}
+							page={page}
+							activeNavLink={activeNavLink}
+						/>
+					) : (
+						""
+					)
+				)}
 			</ul>
 		</nav>
 	)
