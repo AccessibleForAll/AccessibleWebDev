542f30a2fad235935c21bb023bfa3844c1d22200
Christine Belzie <shecoder30@gmail.com>
2023-10-09 19:36:34 -0400

    feat: add two sections to chart entry

--- components/ContentTemplates/ChartsTemplate.tsx
+++ components/ContentTemplates/ChartsTemplate.tsx
@@ -10,9 +10,24 @@ export const ChartsTemplate = () => {
 			<NavPage pageNavigation={chartPageNavigation} />
 			<TemplateSection sectionName="introduction" title="Introduction">
 				<p>
-					{/* TODO: Write introduction */ }
+					The chart is a visual representation of data that uses symbols, lines,
+					or bars to convey information. Whether it's comparing features of two
+					mobile devices on a website, results of a survey on a social media
+					post, or a person's monthly progress in their goal, charts should be
+					conveyed in a way that is easy to for all users to read and
+					understand.
 				</p>
-				</TemplateSection>
+			</TemplateSection>
+			<TemplateSection
+				sectionName="howToAdd"
+				title="How to How To Add Charts to Your Audio?">
+				<p>{/* add paragraph on how to add charts to a website */}</p>
+			</TemplateSection>
+			<TemplateSection
+				sectionName="accessibilityTips"
+				title="Tips to make your charts accessible">
+				<p>{/* add paragraph on how to make your charts accessible */}</p>
+			</TemplateSection>
 		</>
 	)
 }
