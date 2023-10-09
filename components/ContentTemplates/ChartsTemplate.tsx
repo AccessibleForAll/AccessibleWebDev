452eea16bb1ed60fad0b2be6b8d379f8da7b9d67
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2023-10-08 21:24:33 -0400

    Update ChartsTemplate.tsx

--- components/ContentTemplates/ChartsTemplate.tsx
+++ components/ContentTemplates/ChartsTemplate.tsx
@@ -1,5 +1,18 @@
-import { WorkInProgress } from "../WorkInProgress/WorkInProgress"
+import { CodeBlock } from "../CodeBlock/CodeBlock"
+import { NavPage } from "../NavPage/NavPage"
+import { PageUpdated } from "../PageUpdated/PageUpdated"
+import { TemplateSection } from "../TemplateSection/TemplateSection"
+import { chartPageNavigation } from "../../data/pageNavigationLists
 
 export const ChartsTemplate = () => {
-	return <WorkInProgress />
+	return (
+		<>
+			<NavPage pageNavigation={chartPageNavigation} />
+			<TemplateSection sectionName="introduction" title="Introduction">
+				<p>
+					{/* TODO: Write introduction */ }
+				</p>
+				</TemplateSection>
+		</>
+	)
 }
